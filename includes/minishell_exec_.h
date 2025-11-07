051d702b79b4627181585b4a4e78b74203cd4b7d
Nadia Galleze <nadgalle@f6r4s1.clusters.42paris.fr>
2025-11-07 13:25:04 +0100

    MAJ execution with new parsing (AST) - *_old kept for reference - exec_heredoc.c : done , to be tested with AST

---
+++ includes/minishell_exec_.h
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell_exec_.h                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nadgalle <nadgalle@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/25 13:28:09 by sdossa            #+#    #+#             */
+/*   Updated: 2025/11/07 11:48:13 by nadgalle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <signal.h>
+# include <string.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include "libft.h"
+
+/*ENUMERATION TYPE*/
+
+typedef enum e_node_type
+{
+	NODE_COMMAND,
+	NODE_PIPE,
+}	t_node_type;
+
+typedef enum e_redir_type
+{
+	REDIR_IN = 1,
+	REDIR_OUT,
+	REDIR_APPEND,
+	REDIR_HEREDOC
+}	t_redir_type;
+
+/*STUCTURES*/
+
+typedef struct s_redirect
+{
+	int					type;
+	char				*filename;
+	struct s_redirect	*next;
+}	t_redirect;
+
+typedef struct s_command
+{
+	char		**argv;
+	t_redirect	*redir;
+}	t_command;
+
+typedef struct s_node
+{
+	t_node_type		type;
+	struct s_node	*left;
+	struct s_node	*right;
+	t_command		*command;
+}	t_node;
+
+/*MOTHER STRUCTURE*/
+
+typedef struct s_mother_shell
+{
+	t_node	*ast;
+	int		last_status;
+	char	**env;
+}	t_mother_shell;
+
+/****************************************************************/
+/*                       	FUNCTIONS                           */
+/****************************************************************/
+
+extern void	rl_replace_line(const char *str, int n);
+
+extern volatile sig_atomic_t	g_sigint;
+
+#endif
--- srcs/execution/exec_heredoc.c
+++ srcs/execution/exec_heredoc.c
@@ -3,60 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   exec_heredoc.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ssinanis <ssinanis@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nadgalle <nadgalle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 12:31:26 by nadgalle          #+#    #+#             */
-/*   Updated: 2025/02/21 16:21:04 by ssinanis         ###   ########.fr       */
+/*   Updated: 2025/11/07 13:23:53 by nadgalle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-int	ft_open_file(int open_flag, t_command *command)
-{
-	int	fd;
 
-	fd = 0;
-	if (open_flag == 0)
-	{
-		fd = open("srcs/EXECUTION/tmp", O_WRONLY | O_CREAT | O_TRUNC, 0644);
-		if (fd == -1)
-			ft_exit_free("tmp", EXIT_FAILURE, command, NULL);
-	}
-	else if (open_flag == 1)
-	{
-		fd = open("srcs/EXECUTION/tmp", O_RDONLY);
-		if (fd == -1)
-			ft_exit_free("tmp", EXIT_FAILURE, command, NULL);
-	}
-	return (fd);
-}
-
-int	get_heredoc(char *limiter, t_command *command)
-{
-	int		tmpfile_fd;
-	char	*limiter_n;
-
-	tmpfile_fd = ft_open_file(0, command);
-	limiter_n = ft_strjoin(limiter, "\n");
-	read_heredoc_content(limiter_n, tmpfile_fd);
-	free(limiter_n);
-	close(tmpfile_fd);
-	tmpfile_fd = ft_open_file(1, command);
-	return (tmpfile_fd);
-}
-
-void	read_heredoc_content(char *limiter_n, int tmpfile_fd)
+/* 
+** Affiche un prompt "pipe heredoc>" et lit l'entrée utilisateur ligne par ligne
+** jusqu'à atteindre le 'limiter' (par exemple : EOF, STOP, etc.)
+** Chaque ligne est écrite dans le fichier temporaire correspondant.
+*/
+static void	read_heredoc_content(char *limiter_n, int tmpfile_fd)
 {
 	char	*line;
 
 	while (1)
 	{
-		ft_putstr_fd("pipe heredoc>", 1);
+		ft_putstr_fd("pipe heredoc> ", 1);
 		line = get_next_line(0);
 		if (!line)
 		{
-			printf("\n");
+			ft_putstr_fd("\n", 1);
 			break ;
 		}
 		if (ft_strncmp(line, limiter_n, ft_strlen(limiter_n)) == 0)
@@ -67,4 +39,104 @@ void	read_heredoc_content(char *limiter_n, int tmpfile_fd)
 		ft_putstr_fd(line, tmpfile_fd);
 		free(line);
 	}
+}
+
+/* 
+** Ouvre un fichier selon le mode demandé.
+** open_flag == 0 → écriture (O_TRUNC)
+** open_flag == 1 → lecture
+*/
+static int	ft_open_file(char *path, int open_flag, t_command *command)
+{
+	int	fd;
+
+	if (open_flag == 0)
+		fd = open(path, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	else
+		fd = open(path, O_RDONLY);
+	if (fd == -1)
+		ft_exit_free(path, EXIT_FAILURE, command, NULL);
+	return (fd);
+}
+
+/* 
+** Génère un nom de fichier temporaire unique : /tmp/heredoc_X
+** X = index du heredoc courant
+*/
+static char	*ft_get_heredoc_filename(int index)
+{
+	char	*nb;
+	char	*path;
+	char	*tmp;
+
+	nb = ft_itoa(index);
+	if (!nb)
+		return (NULL);
+	tmp = ft_strjoin("/tmp/heredoc_", nb);
+	free(nb);
+	if (!tmp)
+		return (NULL);
+	path = ft_strdup(tmp);
+	free(tmp);
+	return (path);
+}
+
+/* 
+** Crée et écrit un fichier temporaire pour un heredoc donné.
+** Si c'est le dernier heredoc, ouvre aussi le fichier en lecture
+** pour le rediriger plus tard sur STDIN.
+*/
+static int	create_tmp_file(t_command *command, t_redirect *cur, char *tmp_path, int tmpfile_fd)
+{
+	char	*limiter_n;
+	int		write_fd;
+
+	write_fd = ft_open_file(tmp_path, 0, command);
+	limiter_n = ft_strjoin(cur->filename, "\n");
+	if (!limiter_n)
+		ft_exit_free("malloc heredoc limiter", EXIT_FAILURE, command, NULL);
+	read_heredoc_content(limiter_n, write_fd);
+	free(limiter_n);
+	close(write_fd);
+	if (!cur->next || cur->next->type != REDIR_HEREDOC)
+	{
+		if (tmpfile_fd != -1)
+			close(tmpfile_fd);
+		tmpfile_fd = ft_open_file(tmp_path, 1, command);
+	}
+	return (tmpfile_fd);
+}
+
+
+/*
+** Gère tous les heredocs d'une commande :
+** - crée un fichier temporaire pour chacun (/tmp/heredoc_X)
+** - lit leur contenu depuis stdin
+** - garde le dernier heredoc ouvert en lecture (pour stdin)
+** - supprime les fichiers temporaires (unlink)
+*/
+int	get_heredoc(t_command *command)
+{
+	t_redirect	*cur;
+	int			i;
+	int			tmpfile_fd;
+	char		*tmp_path;
+
+	cur = command->redir;
+	i = 0;
+	tmpfile_fd = -1;
+	while (cur)
+	{
+		if (cur->type == REDIR_HEREDOC)
+		{
+			tmp_path = ft_get_heredoc_filename(i++);
+			if (!tmp_path)
+				ft_exit_free("heredoc filename", EXIT_FAILURE, command, NULL);
+			tmpfile_fd = create_tmp_file(command, cur, tmp_path, tmpfile_fd);
+			unlink(tmp_path);
+			free(tmp_path);
+		}
+		cur = cur->next;
+	}
+	return (tmpfile_fd);
 }
